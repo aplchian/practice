832653feb42fb73e9d892ea640a019d110b63306
aplchian <alex.boquis@gmail.com>
2017-01-05 00:04:39 -0500

    finished ex 5

--- functional/reduce/index.js
+++ functional/reduce/index.js
@@ -1,4 +1,4 @@
-const {head,reduce,prop,set,lensProp,compose,tap} = require('ramda')
+const {head,reduce,prop,set,lensProp,compose,tap,curry,ifElse} = require('ramda')
 
 
 // ex3.. Use reduce to return an object of key value pairs from paired arrays,
@@ -19,26 +19,20 @@ const {head,reduce,prop,set,lensProp,compose,tap} = require('ramda')
 // }
 //
 // var x = concat([1,2,3],[4,5,6],[7,8,9])
-//
+// //
 // console.log(x)
 
 
 
 
 // 5. Use reduce to create your own filter function
-
-
-const filter = (fn, list) => {
-  reduce((acc,val) => {
-    if(fn(val)){
-      return [...acc,val]
-    }else {
-      return acc
-    }
-  },[],list)
-}
-
 // filter to even numbers
-var x = filter(v => v % 2 === 0, [1,2,3,4,5,6,7,8,9,10])
+
+
+
+
+const func = curry((fn2,acc,val) => fn2(val) ? [...acc,val] : acc)
+const filter = (fn, list) => reduce(func(fn),[],list)
+var x = filter(v => v % 2 === 1, [1,2,3,4,5,6,7,8,9,10])
 
 console.log(x)
